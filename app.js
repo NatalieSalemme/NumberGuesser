a42156e6340e1b6cf2bdc1ebbf71f4f73a7b7121
Natalie Salemme <nataliesalemme@gmail.com>
2018-08-14 09:59:27 -0700

    Updating number of guesses limit

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-let inputBox = document.getElementById('input');
 let submitButton = document.getElementById('submit');
 let resultsBox = document.getElementById('results-box');
+let helperBox = document.getElementById('help-box');
 
+let helpBox = [];
 
-// function evaluateNum() {
-//
-// }
 let randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
-console.log(randomNumber);
+
 submit.addEventListener('click', function () {
   // console.log(inputBox.value);
-  if(inputBox.value == randomNumber) {
+    if (helpBox.length > 9 && helpBox.length < 11) {
+    // helpBox.classList.add('red-text');
+    helpBox.push('YOU LOST!!!!' + '<br>');
+  } else if(inputBox.value == randomNumber && helpBox.length < 11) {
     resultsBox.innerHTML = 'That is correct!';
-  } else if(inputBox.value > 100 || inputBox.value < 1) {
+  } else if(inputBox.value > 100 || inputBox.value < 1 && helpBox.length < 11) {
     resultsBox.innerHTML = 'Please guess between 1 and 100';
-  } else if(inputBox.value > randomNumber) {
+  } else if(inputBox.value > randomNumber && helpBox.length < 11) {
     resultsBox.innerHTML = 'Too high, guess lower';
-  } else if(inputBox.value < randomNumber) {
-    resultsBox.innerHTMl = 'Too low, guess higher';
+    helpBox.push('x ' + '< ' + inputBox.value + '<br>');
+  } else if (inputBox.value < randomNumber && helpBox.length < 11) {
+    resultsBox.innerHTML = 'Too low, guess higher';
+    helpBox.push('x ' + '> ' + inputBox.value + '<br>');
   }
+
+  helperBox.innerHTML = helpBox.join('');
 });
