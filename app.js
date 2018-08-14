7240e6a9999228a146fea37015d677d2d3dec914
Natalie Salemme <nataliesalemme@gmail.com>
2018-08-14 11:33:48 -0700

    Adding Enter keycode, adding gameover

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
+let inputBox = document.getElementById('input');
 let submitButton = document.getElementById('submit');
 let resultsBox = document.getElementById('results-box');
 let helperBox = document.getElementById('help-box');
+let playButton = document.getElementById('playAgain');
 
 let helpBox = [];
 
 let randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
 
-submit.addEventListener('click', function () {
+
+function gameOver() {
+  let btn = document.createElement("button");
+  let playBtn = document.createTextNode('Play Again?');
+  btn.appendChild(playBtn);
+  playButton.appendChild(btn);
+}
+function guesser() {
   // console.log(inputBox.value);
-    if (helpBox.length > 9 && helpBox.length < 11) {
-    // helpBox.classList.add('red-text');
-    helpBox.push('YOU LOST!!!!' + '<br>');
-  } else if(inputBox.value == randomNumber && helpBox.length < 11) {
+  if(inputBox.value == randomNumber && helpBox.length < 11) {
     resultsBox.innerHTML = 'That is correct!';
-  } else if(inputBox.value > 100 || inputBox.value < 1 && helpBox.length < 11) {
+    helpBox.push('YOU WON!!!!!!' + '<br>' + 'The winning number was ' + inputBox.value + '<br>');
+    inputBox.value = '';
+    gameOver();
+  } else if (helpBox.length == 10) {
+    // helpBox.classList.add('red-text');
+    resultsBox.innerHTML = 'That is incorrect!';
+    helpBox.push('YOU LOST!!!!' + '<br>');
+    inputBox.value = '';
+    gameOver();
+  }  else if(inputBox.value > 100 || inputBox.value < 1) {
     resultsBox.innerHTML = 'Please guess between 1 and 100';
+    inputBox.value = '';
   } else if(inputBox.value > randomNumber && helpBox.length < 11) {
     resultsBox.innerHTML = 'Too high, guess lower';
     helpBox.push('x ' + '< ' + inputBox.value + '<br>');
+    inputBox.value = '';
   } else if (inputBox.value < randomNumber && helpBox.length < 11) {
     resultsBox.innerHTML = 'Too low, guess higher';
     helpBox.push('x ' + '> ' + inputBox.value + '<br>');
+    inputBox.value = '';
   }
 
   helperBox.innerHTML = helpBox.join('');
+}
+
+submit.addEventListener('click', guesser);
+
+inputBox.addEventListener('keydown', (e) => {
+  if(e.keyCode === 13) {
+    guesser();
+  }
 });
